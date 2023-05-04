a3ff7a6252b1fb92c9f11bdc530f8091b1ad3a82
spoeck1995 <daniel.spoeck@hard-soft.at>
2023-05-04 11:44:36 +0200

    extend parent initialize hook

--- src/Model/Table/NotifierBaseTable.php
+++ src/Model/Table/NotifierBaseTable.php
@@ -1,40 +1,43 @@
 <?php
 namespace Notifier\Model\Table;
 
-use Cake\Core\Configure;
-use Cake\Database\Schema\TableSchema;
 use Cake\Database\Type;
-
+use Cake\Core\Configure;
 use App\Model\Table\AppTable;
+use Cake\Database\Schema\TableSchema;
 use Notifier\Database\Type\SerializeType;
 
 class NotifierBaseTable extends AppTable {
-    protected $_serialized = [];
+	protected $_serialized = [];
 
-    /**
-     * {@inheritdoc}
-     */
-    public function initialize(array $config = []) {
-        Type::map('notifier.serialize', SerializeType::class);
+	/**
+	 * {@inheritdoc}
+	 */
+	public function initialize(array $config = []) {
+		parent::initialize($config);
 
-        if (Configure::check('Notifier.table_prefix')) {
-            $this->setTable(Configure::read('Notifier.table_prefix') . $this->getTable());
-        }
-    }
-    /**
-     * Sets the column type for template_vars and headers to json.
-     *
-     * @param TableSchema $schema The table description
-     * @return TableSchemas
-     */
-    protected function _initializeSchema(TableSchema $schema) {
-        $schema = parent::_initializeSchema($schema);
-        
-        if (!empty($this->_serialized)) {
-            foreach ($this->_serialized as $col) {
-                $schema->setColumnType($col, 'notifier.serialize');
-            }
-        }
-        return $schema;
-    }
+		Type::map('notifier.serialize', SerializeType::class);
+
+		if (Configure::check('Notifier.table_prefix')) {
+			$this->setTable(Configure::read('Notifier.table_prefix') . $this->getTable());
+		}
+	}
+
+	/**
+	 * Sets the column type for template_vars and headers to json.
+	 *
+	 * @param TableSchema $schema The table description
+	 * @return TableSchemas
+	 */
+	protected function _initializeSchema(TableSchema $schema) {
+		$schema = parent::_initializeSchema($schema);
+
+		if (!empty($this->_serialized)) {
+			foreach ($this->_serialized as $col) {
+				$schema->setColumnType($col, 'notifier.serialize');
+			}
+		}
+
+		return $schema;
+	}
 }
